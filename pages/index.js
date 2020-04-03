1158bda6e333a868f0f69f5e43c05888c62ca6fe
EunKo <koeun0529@gmail.com>
2020-04-03 18:49:09 +0900

    first commit

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,27 @@
-import React from 'react';
-
-import ProfileCard from '../components/profile-card';
-import styled from 'styled-components';
+import React from "react";
+import ProfileCard from "../src/components/profile-card";
+import styled from "styled-components";
 
 export default function Home() {
-  return (
-    <Wrapper>
-      나는 홈이야
-      <ProfileCard />
-    </Wrapper>
-  );
+	return (
+		<Wrapper>
+			<Subject>CEOS 프론트엔드 운영진</Subject>
+			<ProfileCard />
+		</Wrapper>
+	);
 }
+const Subject = styled.div`
+	font-size: 28px;
+	margin-bottom: 20px;
+	width: 100%;
+`;
 
 const Wrapper = styled.div`
-  background-color: blue;
-  height: 100vh;
+	background-color: rgb(230, 255, 255);
+	padding: 50px 100px;
+	display: flex;
+	flex-direction: row;
+	min-height: 100%;
+	justify-content: space-between;
+	flex-wrap: wrap;
 `;
